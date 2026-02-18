b11c700fbf87707b2a7494eb7ec386764315eaed
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2026-02-18 12:52:45 -0600

    Simplify deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,6 @@
 [CmdletBinding()]
 
 [string]$ResourceGroupName = "tenablevm-test-rg-cnc-01"
-[string]$FxAppZipFileName = "tenablevm.zip"
-
-# Download the function app zip file for deployment
-Invoke-WebRequest -Uri "https://aka.ms/sentinel-TenableVMAzureSentinelConnector310-functionapp" -OutFile $FxAppZipFileName
 
 $DeploymentResults = New-AzResourceGroupDeployment `
     -Name "TenableVMFxAppDeployment" `
@@ -14,17 +10,5 @@ $DeploymentResults = New-AzResourceGroupDeployment `
     -Verbose
 
 if ($DeploymentResults.ProvisioningState -eq 'Succeeded') {
-    # Flexible Consumption model doesn't support specifying the zip file as an environment variable during deployment.
-    # Therefore, we need to deploy the zip file separately after the function app is created.
-
-    # Ensure we're using the same subscription for AZ CLI as for the Az PS modules
-    az account set --subscription "$((Get-AzContext).Subscription.Id)"
-
-    # Deploy the function app zip file
-    az functionapp deployment source config-zip `
-        --name "$($DeploymentResults.Outputs.functionAppName.Value)" `
-        --resource-group $ResourceGroupName `
-        --src $FxAppZipFileName
-
     Write-Host "🔥 Deployment successful!"
 }
