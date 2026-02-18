342ba3cc187196e19545cb901e06d789be6c15f0
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2026-02-17 19:48:03 -0600

    Deploy script updates

--- src/scripts/Deploy-FunctionApp.ps1
+++ src/scripts/Deploy-FunctionApp.ps1
@@ -16,8 +16,16 @@ param (
 # Version 3.11: https://aka.ms/sentinel-TenableVMAzureSentinelConnector-functionapp
 # Download the function app zip file for deployment
 Invoke-WebRequest -Uri $FxAppSource -OutFile $FxAppZipFileName
-az login --identity --client-id $ClientId
-az functionapp deployment source config-zip `
+
+& winget install -e -h -s winget --id Microsoft.AzureCLI
+
+# Get-Command az might not return anything because "az" isn't in the path yet
+$AZCliPath = Get-Command az | Select-Object -ExpandProperty Source
+# Alternative:
+#$AZCliPath = "$($Env:ProgramFiles)\Microsoft SDKs\Azure\CLI2\wbin\az"
+
+& "$AZCliPath" login --identity --client-id $ClientId
+& "$AZCliPath" functionapp deployment source config-zip `
     --name "$FunctionAppName" `
     --resource-group $ResourceGroupName `
     --src $FxAppZipFileName
